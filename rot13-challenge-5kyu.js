25575577614b638ed3d7042f4ddc55b267949fcf
spichay <seanpichay1015@gmail.com>
2024-11-26 10:41:07 -0500

    completed 8 challenges need to comment

---
+++ rot13-challenge-5kyu.js
@@ -0,0 +1,35 @@
+/**
+ * How can you tell an extrovert from an introvert at NSA?
+ * Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+ *
+ * I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+ * According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+ *
+ * For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+ *
+ * Test examples:
+ *
+ * "EBG13 rknzcyr." -> "ROT13 example."
+ *
+ * "This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
+ */
+
+function rot13(str) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let alphabetUpperCase = alphabet.toUpperCase();
+    let result = '';
+    for (let char of str){
+        if(alphabet.includes(char)){
+            let index = alphabet.indexOf(char);
+            let newIndex = (index + 13) % 26;
+            result += alphabet[newIndex];
+        }else if (alphabetUpperCase.includes(char)){
+            let index = alphabetUpperCase.indexOf(char);
+            let newIndex = (index + 13) % 26;
+            result += alphabetUpperCase[newIndex];
+        }else {
+            result += char;
+        }
+    }
+    return result;
+}
